5e416868a452d993539f70a5d1d5c2c0277e6e78
ron on mbp <ronmbp@ronchambers.com>
2018-10-20 18:43:32 -0700

    Updates

---
+++ precache-manifest.6a0cb823b71802c56e9a927b0fb558a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9117fcced3c3ee934457",
+    "url": "/clickyReact/static/js/runtime~main.9117fcce.js"
+  },
+  {
+    "revision": "93fd3cca3a35a1f2665d",
+    "url": "/clickyReact/static/js/main.93fd3cca.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clickyReact/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "93fd3cca3a35a1f2665d",
+    "url": "/clickyReact/static/css/main.f20c4b87.chunk.css"
+  },
+  {
+    "revision": "c0e8a39e9fd3adbf5aee457d4961e275",
+    "url": "/clickyReact/index.html"
+  }
+];
